39889a0e9fde0281d1b942694a9cf98e79f072f7
acdelgrossi <fiidohug@gmail.com>
2026-09-25 09:10:00 -0400

    style: corrige indentação do item do menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -138,7 +138,7 @@
   const menuItems = [
     { title: 'Dashboard', icon: 'mdi-view-dashboard', value: 'dashboard', to: '/dashboard' },
     { title: 'Cidadãos', icon: 'mdi-account-group', value: 'citizens', to: '/' },
-   { title: 'Simulação PEC', icon: 'mdi-monitor-eye', value: 'simulacao-pec', to: '/simulacao-pec' },
+    { title: 'Simulação PEC', icon: 'mdi-monitor-eye', value: 'simulacao-pec', to: '/simulacao-pec' },
   ]
 
   const breadcrumbs = computed(() => {
